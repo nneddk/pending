12facfe7798a696e82fb19c25c7b8dcf61e49d3d
nneddk <meljandre.mc@gmail.com>
2023-01-28 14:49:56 +0800

    api and more skeleton work added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from "./components/Header";
 import './stylesheet/App.css';
-//import './stylesheet/Clean.css';
+import './stylesheet/Clean.css';
 
 const App = () =>{
   return(
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React, {useState} from "react";
+import '../stylesheet/Header.css'
+/* API: https://v6.exchangerate-api.com/v6/e667579449fad91748dcaa24/latest/PHP */
 const Header = () =>{
-    const [currentBalance, setCurrentBalance] = useState(0.00);
+    const [currentBalance, setCurrentBalance] = useState(1000.00);
     return(
-        <div id = 'header'>
-            {currentBalance}
+        <div className="header">
+            <button className="peso-usd">$</button>
+            <div className="current-balance-div">{currentBalance}</div>
         </div>
     );
 };
