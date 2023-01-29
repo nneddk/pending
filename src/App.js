568f8b2dca6208d662889591b9fceaac56259e88
nneddk <meljandre.mc@gmail.com>
2023-01-29 16:26:07 +0800

    Main content page created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import Header from "./components/Header";
+import Header from './components/Header';
+import Main from './components/Main'
 import './stylesheet/App.css';
 import './stylesheet/Clean.css';
 
@@ -7,6 +8,7 @@ const App = () =>{
   return(
     <>
       <Header/>
+      <Main/>
     </>
   );
 };
