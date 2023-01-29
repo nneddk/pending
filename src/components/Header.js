03e870e9eb2054caccfcbf3710c9ce00c519c114
nneddk <meljandre.mc@gmail.com>
2023-01-29 16:26:56 +0800

    header visibility on scroll added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,29 @@
 import React, {useState} from "react";
-import '../stylesheet/Header.css'
+import '../stylesheet/components/Header.css'
 /* API: https://v6.exchangerate-api.com/v6/e667579449fad91748dcaa24/latest/PHP */
+
+window.addEventListener('scroll' ,(e) => {
+    const headerExtended = document.querySelector('.header');
+    if(window.scrollY > 0){
+        headerExtended.style.height = '0';
+    }
+    
+    if(window.scrollY === 0){
+        headerExtended.style.height = '80vh';
+        
+    }
+});
+
 const Header = () =>{
+    
+    
     const [currentBalance, setCurrentBalance] = useState(1000.00);
     return(
         <div className="header">
-            <button className="peso-usd">$</button>
-            <div className="current-balance-div">{currentBalance}</div>
+            
+            <div className="current-balance-div">
+                {currentBalance}
+                </div>
         </div>
     );
 };
